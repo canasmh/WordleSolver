9769879d21ab91b973dfd288d590c18ae1dc78a7
canasmh <canasmh@yahoo.com>
2022-03-28 13:43:42 -0400

    Set the game app as a class attribute. Also get the letters from the keyboard elements

--- wordle.py
+++ wordle.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from english_words import english_words_lower_alpha_set
 import time
 
 
@@ -13,18 +12,25 @@ class WordleDriver:
         self.driver = webdriver.Chrome(service=self.service)
         self.url = 'https://www.nytimes.com/games/wordle/index.html'
         self.driver.get(self.url)
+        self.game_app = self.expand_shadow_element(self.driver.find_element(By.TAG_NAME, "game-app"))
         time.sleep(3)
         self.close_instructions()
 
     def close_instructions(self):
-        root1 = self.expand_shadow_element(self.driver.find_element(By.TAG_NAME, "game-app"))
-        root2 = self.expand_shadow_element(root1.find_element(By.TAG_NAME, "game-modal"))
+        root2 = self.expand_shadow_element(self.game_app.find_element(By.TAG_NAME, "game-modal"))
         close_icon = root2.find_element(By.CSS_SELECTOR, 'div.close-icon')
         close_icon.click()
 
     def expand_shadow_element(self, element):
         shadow_root = self.driver.execute_script('return arguments[0].shadowRoot', element)
         return shadow_root
+
+    def get_keyboard(self):
+        keyboard = self.game_app.find_element(By.TAG_NAME, "game-keyboard")
+        keyboard_shadow = self.expand_shadow_element(keyboard)
+        letters = keyboard_shadow.find_elements(By.TAG_NAME, 'button')
+
+        return letters
 
 
 if __name__ == "__main__":
